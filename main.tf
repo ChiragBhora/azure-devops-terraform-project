51fb49ff1d677b024dd8e62d66a7df47ef376162
ChiragBhora <chiragbhora1@gmail.com>
2026-05-20 06:55:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "devops-rg-Chirag76"
+  name     = "devops-rg-chirag2"
   location = "Central India"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "chiragdevopsstorage76"
+  name                     = "chiragdevopsstorage2"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
