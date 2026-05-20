2a2af771924400a18f8e4d2ab71a110e388f5cc8
ChiragBhora <chiragbhora1@gmail.com>
2026-05-20 08:50:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = "chiragstorage001x"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
