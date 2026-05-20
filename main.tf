2bd3444e1588ce60ef578cc468e6117c48a67589
ChiragBhora <chiragbhora1@gmail.com>
2026-05-20 07:06:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,10 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "chiragfreshrg001"
+  name     = "rohitfreshrg001"
   location = "Central India"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "chiragstorage001x"
+  name                     = "rohitstorage001x"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
