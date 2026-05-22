b67e517ddde916ee459a4ed322f90a674b398923
ChiragBhora <chiragbhora1@gmail.com>
2026-05-22 12:00:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,10 +22,11 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 resource "azurerm_public_ip" "publicip" {
-  name                = "chirag-public-ip"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  name                = "chirag-public-ip-01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 resource "azurerm_network_security_group" "nsg" {
   name                = "chirag-nsg"
