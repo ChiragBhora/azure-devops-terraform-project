1cded6da44e73cfde5e1d246c8f4126490474d98
ChiragBhora <chiragbhora1@gmail.com>
2026-05-22 12:08:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,37 @@
 data "azurerm_resource_group" "rg" {
-  name = "chiragrg12"
+  name = "chiragfreshrg001"
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "chiragstorage12"
+  name                     = "chiragstorage001x"
   resource_group_name      = data.azurerm_resource_group.rg.name
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "chirag-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
+
 resource "azurerm_subnet" "subnet" {
   name                 = "chirag-subnet"
   resource_group_name  = data.azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
 resource "azurerm_public_ip" "publicip" {
   name                = "chirag-public-ip-01"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+
 resource "azurerm_network_security_group" "nsg" {
   name                = "chirag-nsg"
   location            = data.azurerm_resource_group.rg.location
@@ -45,6 +49,7 @@ resource "azurerm_network_security_group" "nsg" {
     destination_address_prefix = "*"
   }
 }
+
 resource "azurerm_network_interface" "nic" {
   name                = "chirag-nic"
   location            = data.azurerm_resource_group.rg.location
@@ -57,6 +62,7 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "chirag-vm"
   resource_group_name = data.azurerm_resource_group.rg.name
